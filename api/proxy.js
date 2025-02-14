5f460352d903cce5b30eab301cc4e04d61528e98
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2025-02-14 16:06:16 -0500

    Create proxy.js

---
+++ api/proxy.js
@@ -0,0 +1,27 @@
+export default async function handler(req, res) {
+  const url = new URL(req.url);
+  const targetUrl = url.searchParams.get("url");
+
+  if (!targetUrl) {
+    return res.status(400).send("Missing 'url' parameter.");
+  }
+
+  const response = await fetch(targetUrl, {
+    headers: {
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.9",
+      "Accept-Encoding": "gzip, deflate, br",
+      "Cache-Control": "no-cache",
+      "Pragma": "no-cache",
+      "Connection": "keep-alive",
+      "Upgrade-Insecure-Requests": "1",
+    },
+  });
+
+  const contentType = response.headers.get("content-type");
+  res.setHeader("Content-Type", contentType);
+  
+  const body = await response.text();
+  res.send(body);
+}
