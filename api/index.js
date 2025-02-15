b86b61e995f8a185f30201fdabe4dbed8e3bbaff
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2025-02-15 16:13:05 -0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,13 +10,17 @@ module.exports = async (req, res) => {
 
   try {
     const response = await axios.get(targetUrl, {
+      responseType: 'arraybuffer',
       headers: {
         'User-Agent': req.headers['user-agent'],
+        'Accept': '*/*',
       },
     });
 
+    res.setHeader('Content-Type', response.headers['content-type']);
     res.status(response.status).send(response.data);
   } catch (error) {
+    console.error(error);
     res.status(500).send('Error proxying request');
   }
 };
