4d46b2ce0fb7ed31a638e8b4581b6fdb062cf3b6
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2025-02-17 11:56:52 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("fetch", event => {
-    let url = new URL(event.request.url);
-    
-    if (url.pathname.startsWith("/proxy/")) {
-        let targetUrl = atob(url.pathname.replace("/proxy/", ""));
-        event.respondWith(fetch(targetUrl, { mode: "cors" }).catch(() => fetch(targetUrl)));
-    }
-});
