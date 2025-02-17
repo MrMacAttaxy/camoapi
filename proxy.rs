1ba58d1d63084597bfdae973509a569051c93d6a
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2025-02-17 11:53:51 -0500

    Delete proxy.rs

--- proxy.rs
+++
@@ -1,9 +0,0 @@
-extern crate reqwest;
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub async fn fetch_url(url: String) -> Result<JsValue, JsValue> {
-    let response = reqwest::get(&url).await.unwrap();
-    let body = response.text().await.unwrap();
-    Ok(JsValue::from_str(&body))
-}
