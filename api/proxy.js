cf843c8b1732addb78b7206fddadce341911c1a2
MrMacAttaxy <116235140+MrMacAttaxy@users.noreply.github.com>
2025-02-26 00:00:38 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -54,6 +54,9 @@ export default async function handler(req, res) {
 
       res.setHeader('Content-Type', 'text/html');
       res.status(response.status).send(htmlContent);
+    } else if (contentType.includes('image/') || contentType.includes('video/')) {
+      res.setHeader('Content-Type', contentType);
+      res.status(response.status).send(response.data);
     } else {
       res.setHeader('Content-Type', contentType);
       res.status(response.status).send(response.data);
